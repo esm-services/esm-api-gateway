9fb49c521db1e5de53c55a6392ff8bb338977e3b
Durgesh RAI <drai548@gmail.com>
2017-08-27 18:04:58 +0530

    createt api gateway

---
+++ src/main/java/com/esm/apigateway/service/config/OAuthConfiguration.java
@@ -0,0 +1,23 @@
+package com.esm.apigateway.service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+import com.esm.apigateway.service.security.CustomUserInfoTokenServices;
+
+@Configuration
+public class OAuthConfiguration {
+
+	@Autowired
+	private ResourceServerProperties sso;
+
+	@Bean
+	@Primary
+	public ResourceServerTokenServices tokenServices() {
+		return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
+	}
+}
---
+++ src/main/java/com/esm/apigateway/service/security/ResourceServerConfiguration.java
@@ -0,0 +1,30 @@
+package com.esm.apigateway.service.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+	
+	private final ResourceServerTokenServices tokenServices;
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/oauth/token").permitAll()
+		.antMatchers("/oauth/authorize").permitAll()
+		.and().csrf().disable();
+	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenServices(tokenServices).stateless(false);
+	}
+}
